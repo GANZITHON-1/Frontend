f81d9ca24f5e1d63d36df249e1832018de77ae2f
iyeonggyu0 <105581475+iyeonggyu0@users.noreply.github.com>
2025-11-06 23:10:44 +0900

    [docs] app 스크롤 탑 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import SplashPage from "./pages/splash/entry/SplashPage";
+import ScrollToTop from "./hook/useScrollTop";
 
 function App() {
   return (
