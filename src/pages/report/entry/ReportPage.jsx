ec5c31f171ad8001bcd06ac35cf03e4b702efe3c
taesung <jeayagosu@gmail.com>
2025-11-09 11:24:07 +0900

    [test] 임시 커밋

---
+++ src/pages/report/entry/ReportPage.jsx
@@ -0,0 +1,11 @@
+import NavigationBar from "../../../component/NavigationBar/NavigationBar";
+
+export function ReportPage() {
+  return (
+    <div className="navigationbar">
+      <NavigationBar title="제보하기" />
+    </div>
+  );
+}
+
+export default ReportPage;
