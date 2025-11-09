0876217bd0eaf3e54feb4f5084d758f356fece67
twolim <happygrove8@gmail.com>
2025-11-09 22:27:43 +0900

    [feat] 온보딩 캐러셀

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import SplashPage from "./pages/splash/entry/SplashPage";
 import ScrollToTop from "./hook/useScrollTop";
 import ReportPage from "./pages/report/entry/ReportPage";
+import OnboardingPage from "./pages/onboarding/entry/OnboardingPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<SplashPage />} />
           <Route path="/report" element={<ReportPage />} />
+          <Route path="/onboarding" element={<OnboardingPage />} />
         </Routes>
       </div>
     </div>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  server: {
+    host: "0.0.0.0", // 💡 모든 네트워크 인터페이스에서 접근 가능하도록 설정
+    port: 5173, // 사용하는 포트 번호
+  },
+});
