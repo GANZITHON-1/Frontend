a3520778cf2a3606dc4cdbc7445c0b4b322cb607
taesung <jeayagosu@gmail.com>
2025-11-09 11:19:43 +0900

    [feat] 공용 디자인 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import SplashPage from "./pages/splash/entry/SplashPage";
 import ScrollToTop from "./hook/useScrollTop";
+import ReportPage from "./pages/report/entry/ReportPage";
 
 function App() {
   return (
-    <>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<SplashPage />} />
-      </Routes>
-    </>
+    <div className="app-container">
+      <div className="scroll-area">
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<SplashPage />} />
+          <Route path="/report" element={<ReportPage />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
